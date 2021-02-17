80284cd9b41ebfe459af2b4a06447f78f1ce13ae
nositelshtanov <74177109+nositelshtanov@users.noreply.github.com>
2021-02-17 08:52:43 +0300

    90 deg turn_matrix

--- matrix_turn.py
+++ matrix_turn.py
@@ -2,8 +2,11 @@ import random
 
 n = int(input('кол во строк и столбцов: '))
 a = [[random.randint(-20,20) for j in range(n)] for i in range(n)]
+start = 0
+end = n
 matrix = []
-temp_matrix = []
+temp = []
+rezult_matrix = []
 
 
 for i in range(n):
@@ -11,13 +14,23 @@ for i in range(n):
         print('{:4.0f}'.format(a[i][j]), end=' ')
     print()
 
-print(a)
-
 for i in range(n):
     for j in range(n-1, -1, -1):
         matrix.append(a[j][i])
-print(matrix)
 
+for i in range(n):
+    temp = matrix[start:end]
+    rezult_matrix.append(temp)
+    temp = []
+    start += n 
+    end += n
+
+print()
+
+for i in range(n):
+    for j in range(n):
+        print('{:4.0f}'.format(rezult_matrix[i][j]), end=' ')
+    print()
 
 
 
